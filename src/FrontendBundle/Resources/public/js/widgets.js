4758c198420cdfafb37accb8dd4acef1f235422e
Jorge Luis Betancourt <betancourt.jorge@gmail.com>
2016-06-11 05:39:12 -0400

    Better organization for the template hierarchy in the frontend Fix JS scroll bug on the other pages of the frontend

--- src/FrontendBundle/Resources/public/js/widgets.js
+++ src/FrontendBundle/Resources/public/js/widgets.js
@@ -26,4 +26,19 @@ $(document).ready(function () {
             next.children(':first-child').clone().appendTo($(this));
         }
     });
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 80
+    });
+
+    // Page scrolling feature
+    $('a.page-scroll').bind('click', function (event) {
+        var link = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(link.attr('href')).offset().top - 50
+        }, 500);
+        event.preventDefault();
+        $("#navbar").collapse('hide');
+    });
 });
