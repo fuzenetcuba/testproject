fd138b1c25b4ded9910ac1e4f65982ff9576b719
Yulio Aleman Jimenez <yulioaj290@gmail.com>
2016-08-17 00:26:27 -0400

    Added link to the Plaza Map Page into the search panel of the directory page. Fixed bugs about navbar scrolling effects. Fixed bugs for container-image CSS class.

--- src/FrontendBundle/Resources/public/js/app.js
+++ src/FrontendBundle/Resources/public/js/app.js
@@ -43,39 +43,39 @@ $(document).ready(function () {
     })
 });
 
-var cbpAnimatedHeader = (function () {
-    var docElem = document.documentElement,
-        header = document.querySelector('.navbar-default'),
-        didScroll = false,
-        changeHeaderOn = 200;
-
-    function init() {
-        window.addEventListener('scroll', function (event) {
-            if (!didScroll) {
-                didScroll = true;
-                setTimeout(scrollPage, 250);
-            }
-        }, false);
-    }
-
-    function scrollPage() {
-        var sy = scrollY();
-        if (sy >= changeHeaderOn) {
-            $(header).addClass('navbar-scroll')
-        }
-        else {
-            $(header).removeClass('navbar-scroll')
-        }
-        didScroll = false;
-    }
-
-    function scrollY() {
-        return window.pageYOffset || docElem.scrollTop;
-    }
-
-    init();
-
-})();
+// var cbpAnimatedHeader = (function () {
+//     var docElem = document.documentElement,
+//         header = document.querySelector('.navbar-default'),
+//         didScroll = false,
+//         changeHeaderOn = 200;
+//
+//     function init() {
+//         window.addEventListener('scroll', function (event) {
+//             if (!didScroll) {
+//                 didScroll = true;
+//                 setTimeout(scrollPage, 250);
+//             }
+//         }, false);
+//     }
+//
+//     function scrollPage() {
+//         var sy = scrollY();
+//         if (sy >= changeHeaderOn) {
+//             $(header).addClass('navbar-scroll')
+//         }
+//         else {
+//             $(header).removeClass('navbar-scroll')
+//         }
+//         didScroll = false;
+//     }
+//
+//     function scrollY() {
+//         return window.pageYOffset || docElem.scrollTop;
+//     }
+//
+//     init();
+//
+// })();
 
 // Activate WOW.js plugin for animation on scrol
 new WOW().init();
