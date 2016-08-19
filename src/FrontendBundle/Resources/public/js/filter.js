82982882a6fd8d840227c9a2d3b7ca2c7f0acd5f
Jorge Luis Betancourt <jlbetancourt@uci.cu>
2016-08-19 00:42:13 -0400

    Adding auto-submit feature for the the search input in the business directory using an opportunistic implementation: when the user stop typing then the AJAX request is fired

--- src/FrontendBundle/Resources/public/js/filter.js
+++ src/FrontendBundle/Resources/public/js/filter.js
@@ -1,3 +1,48 @@
+/*
+ * Fires callback when a user has finished typing. This is determined by the time elapsed
+ * since the last keystroke and timeout parameter or the blur event--whichever comes first.
+ *
+ * @callback: function to be called when even triggers
+ * @timeout:  (default=1000) timeout, in ms, to to wait before triggering event if not
+ * caused by blur.
+ */
+;(function ($) {
+    $.fn.extend({
+        doneTyping: function (callback, timeout) {
+            timeout = timeout || 1e3; // 1 second default timeout
+            var timeoutReference,
+                doneTyping = function (el) {
+                    if (!timeoutReference) return;
+                    timeoutReference = null;
+                    callback.call(el);
+                };
+            return this.each(function (i, el) {
+                var $el = $(el);
+                // Chrome Fix (Use keyup over keypress to detect backspace)
+                // thank you @palerdot
+                $el.is(':input') && $el.on('keyup keypress paste', function (e) {
+                    // This catches the backspace button in chrome, but also prevents
+                    // the event from triggering too preemptively. Without this line,
+                    // using tab/shift+tab will make the focused element fire the callback.
+                    if (e.type == 'keyup' && e.keyCode != 8) return;
+
+                    // Check if timeout has been set. If it has, "reset" the clock and
+                    // start over again.
+                    if (timeoutReference) clearTimeout(timeoutReference);
+                    timeoutReference = setTimeout(function () {
+                        // if we made it here, our timeout has elapsed. Fire the
+                        // callback
+                        doneTyping(el);
+                    }, timeout);
+                }).on('blur', function () {
+                    // If we can, fire the event since we're leaving the field
+                    doneTyping(el);
+                });
+            });
+        }
+    });
+})(jQuery);
+
 $(document).ready(function () {
     $('.hide-filters').on('click', function () {
         console.log('toggle view');
@@ -41,4 +86,8 @@ $(document).ready(function () {
     $('.filter-deals form select').on('change', function () {
         $(this).parent().parent().submit();
     });
+
+    $('.filter-deals input[type="search"]').doneTyping(function () {
+        $(this).parent().parent().submit();
+    }, 300);
 });
