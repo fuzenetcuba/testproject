c5d1752076242c2e32dc6828bdbc3b36602aba0f
Jorge Luis Betancourt <betancourt.jorge@gmail.com>
2017-06-08 00:33:30 +0200

    Merge local branch 'feature/backend-press' into master

---
+++ src/BackendBundle/Controller/PressPostController.php
@@ -0,0 +1,256 @@
+<?php
+
+namespace BackendBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;//------------
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use BackendBundle\Entity\PressPost;
+use BackendBundle\Form\PressPostType;
+
+/**
+ * PressPost controller.
+ *
+ */
+class PressPostController extends Controller
+{
+    /**
+     * Lists all PressPost entities.
+     *
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $dql = "SELECT e FROM BackendBundle:PressPost e ORDER BY e.id ASC";
+        $query = $em->createQuery($dql);
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+                $query, $request->query->get('page', 1), 5
+        );
+
+    return $this->render('presspost/index.html.twig', array(
+        'entities' => $pagination,
+    ));
+    }
+
+    /**
+     * Find PressPost entities that match with the criteria.
+     *
+     */
+    public function findAction(Request $request)
+    {
+        $find = $request->get('find-form-text');
+        
+        if ($find) {
+            $em = $this->getDoctrine()->getManager();
+
+            $dql = "SELECT e FROM BackendBundle:PressPost e WHERE "
+                    . "e.id LIKE '%" . $find . "%' OR "
+                    . "e.id LIKE '%" . $find . "%' "
+                    . "ORDER BY e.id ASC";
+            $query = $em->createQuery($dql);
+
+            $paginator = $this->get('knp_paginator');
+            $pagination = $paginator->paginate(
+                    $query, $request->query->get('page', 1), 5
+            );
+
+            return $this->render('presspost/index.html.twig', array(
+                'entities' => $pagination,
+                'textFind' => $find
+            ));
+        } else {
+            return $this->redirect($this->generateUrl('press'));
+        }
+    }
+
+    /**
+     * Displays a form to create a new PressPost entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $entity = new PressPost();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            // Mostrando mensaje
+            $this->get('session')->getFlashBag()->add('success', 'The presspost was created successfully.');
+
+            if ($form->get('submitback')->isClicked()) {
+                return $this->redirect($this->generateUrl('press_new'));
+            } else {
+            return $this->redirectToRoute('press_show', array('id' => $entity->getId()));            }
+
+        }
+
+    return $this->render('presspost/new.html.twig', array(
+        'entity' => $entity,
+        'form' => $form->createView(),
+    ));
+    }
+
+            /**
+        * Creates a form to create a PressPost entity.
+        *
+        * @param PressPost $entity The entity
+        *
+        * @return \Symfony\Component\Form\Form The form
+        */
+        private function createCreateForm(PressPost $entity)
+        {
+            $form = $this->createForm('BackendBundle\Form\PressPostType', $entity, array(
+                'action' => $this->generateUrl('press_new'),
+                'method' => 'POST',
+            ));
+
+            $form->add('submit', SubmitType::class, array('label' => 'Create'));
+            $form->add('submitback', SubmitType::class, array('label' => 'Create & Back'));
+
+            return $form;
+        }
+    
+    /**
+     * Finds and displays a PressPost entity.
+     *
+     */
+    public function showAction(PressPost $entity)
+    {
+        $deleteForm = $this->createDeleteForm($entity);
+
+        return $this->render('presspost/show.html.twig', array(
+            'entity' => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing PressPost entity.
+     *
+     */
+    public function editAction(Request $request, PressPost $entity)
+    {
+        $deleteForm = $this->createDeleteForm($entity);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+    
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            // Mostrando mensaje
+            $this->get('session')->getFlashBag()->add('success', 'The presspost was updated successfully.');
+            return $this->redirectToRoute('press_show', array('id' => $entity->getId()));
+        }
+
+    return $this->render('presspost/edit.html.twig', array(
+        'entity' => $entity,
+        'form' => $editForm->createView(),
+        'delete_form' => $deleteForm->createView(),
+    ));
+    }
+
+    /**
+    * Creates a form to edit a PressPost entity.
+    *
+    * @param PressPost $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(PressPost $entity)
+    {
+        $form = $this->createForm('BackendBundle\Form\PressPostType', $entity, array(
+            'action' => $this->generateUrl('press_edit', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', SubmitType::class, array('label' => 'Update'));
+
+        return $form;
+    }
+
+    /**
+     * Deletes a PressPost entity.
+     *
+     */
+    public function deleteAction(Request $request, PressPost $entity)
+    {
+        $form = $this->createDeleteForm($entity);
+        $form->handleRequest($request);
+
+        $em = $this->getDoctrine()->getManager();
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find PressPost entity.');
+        }
+
+        $em->remove($entity);
+        $em->flush();
+
+        // Mostrando mensaje
+        $this->get('session')->getFlashBag()->add('success', 'The presspost was deleted successfully.');
+        return $this->redirect($this->generateUrl('press'));
+
+
+
+    }
+
+    /**
+    * Creates a form to delete a PressPost entity.
+    *
+    * @param PressPost $entity The PressPost entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createDeleteForm(PressPost $entity)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('press_delete', array('id' => $entity->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+    /**
+     * Do several batch actions over PressPost entities.
+     *
+     */
+    public function batchAction(Request $request)
+    {
+        $action = $request->get('batch_action_do');
+        $ids = $request->get('batch_action_checkbox');
+        $recordsSelected = false;
+        
+        if ($ids) {
+            $em = $this->getDoctrine()->getManager();
+
+            if ($action == "delete") {
+                foreach ($ids as $id) {
+                    $entity = $em->getRepository('BackendBundle:PressPost')->find($id);
+
+                    if (!$entity) {
+                        throw $this->createNotFoundException('Unable to find PressPost entity.');
+                    } else {
+                        $em->remove($entity);
+                        $recordsSelected = true;
+                    }
+                }
+                if ($recordsSelected) {
+                    $this->get('session')->getFlashBag()->add('success', 'PressPosts deleted successfully.');
+                }
+            }
+            $em->flush();
+        }
+
+
+        return $this->redirect($this->generateUrl('press'));
+    }
+}
---
+++ src/BackendBundle/Entity/PressPost.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace BackendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * PressPost
+ *
+ * @ORM\Table(name="press_post")
+ * @ORM\Entity(repositoryClass="BackendBundle\Repository\PressPostRepository")
+ * @Vich\Uploadable
+ */
+class PressPost
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="imave", type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @var File
+     *
+     * This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="press_image", fileNameProperty="image",
+     *      groups={"creation"}
+     * )
+     * @Assert\Image(
+     *     minWidth="16",
+     *     maxWidth="1500",
+     *     minWidthMessage="The image must have a with between 16 and 1500 pixels",
+     *     maxWidthMessage="The image must have a with between 16 and 1500 pixels",
+     *     minHeight="16",
+     *     maxHeight="1500",
+     *     minHeightMessage="The image must have a height between 16 and 1500 pixels",
+     *     maxHeightMessage="The image must have a height between 16 and 1500 pixels",
+     *     maxSize="1M",
+     *     maxSizeMessage="The image must have 1 MB (megabytes) or less"
+     * )
+     */
+    private $imageFile;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="video", type="string", length=255, nullable=true)
+     */
+    private $video;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="text", length=255)
+     */
+    private $text;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set imave
+     *
+     * @param string $image
+     *
+     * @return PressPost
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get imave
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set video
+     *
+     * @param string $video
+     *
+     * @return PressPost
+     */
+    public function setVideo($video)
+    {
+        $this->video = $video;
+
+        return $this;
+    }
+
+    /**
+     * Get video
+     *
+     * @return string
+     */
+    public function getVideo()
+    {
+        return $this->video;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return PressPost
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\File\File
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\File\File $imageFile
+     */
+    public function setImageFile($imageFile)
+    {
+        $this->imageFile = $imageFile;
+    }
+}
