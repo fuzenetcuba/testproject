fed23ef6fa4b1e6302033051544345ed1d06121c
Yulio Aleman Jimenez <yulioaj290@gmail.com>
2017-06-23 10:33:37 -0400

    Updated pagination function with styles and new method.

--- src/BackendBundle/Resources/public/js/components/pagination.js
+++ src/BackendBundle/Resources/public/js/components/pagination.js
@@ -1,22 +1,47 @@
 Vue.component('pagination', {
-    template: '<div class="m-l-lg"><select v-model="currentItem" number @change="updatePagination" class="minimal" name="pagination"><option v-for="value in options"value="{{value}}">{{value}}</option></select></div>',
+    template: '<div class="form-group pagination m-l-lg">' +
+    '<select v-model="currentItem" number @change="updatePagination" id="select_pagination" class="form-control minimal" name="pagination">' +
+    '<option v-for="value in options"value="{{value}}">{{value}}</option>' +
+    '</select>' +
+    '</div>',
 
-    data: function () {
-        return {
-            options: [5, 10, 20, 25, 50],
-            currentItem: 10,
-            url: new Url
-        }
-    },
+    // data: function () {
+    //     return {
+    //         options: [5, 10, 20, 25, 50],
+    //         currentItem: 10,
+    //         url: new Url
+    //     }
+    // },
+    //
+    // created: function () {
+    //     this.currentItem = this.url.query.limit || 10;
+    // },
+    //
+    // methods: {
+    //     updatePagination: function () {
+    //         this.url.query.limit = this.currentItem;
+    //         window.location = this.url.toString();
+    //     }
+    // }
+});
 
-    created: function () {
-        this.currentItem = this.url.query.limit || 10;
-    },
+$(document).ready(function () {
 
-    methods: {
-        updatePagination: function () {
-            this.url.query.limit = this.currentItem;
-            window.location = this.url.toString();
-        }
-    }
-})
+    var url = new Url;
+
+    $("#select_pagination").select2({
+        minimumResultsForSearch: Infinity,
+        data: [
+            {id: '5', text: '5'},
+            {id: '10', text: '10'},
+            {id: '20', text: '20'},
+            {id: '25', text: '25'},
+            {id: '50', text: '50'},
+        ],
+        val: '10'
+    }).on("select2:select", function (e) {
+        url.query.limit = $("#select_pagination").val();
+        window.location = url.toString();
+
+    }).val(url.query.limit || "10").trigger("change");
+});
